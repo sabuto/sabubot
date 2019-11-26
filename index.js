0706fc22f6ecbd5f3f9480c26b581d5a73f78f06
Robert Dunne <robe_dunne@hotmail.com>
2019-11-26 16:17:26 +0000

    update

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+    app.log(context)
+    // const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    // return context.github.issues.createComment(issueComment)
   });
 
   app.on('push', async context => {
