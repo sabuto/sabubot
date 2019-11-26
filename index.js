798fb342b333233a8c0a9b8898ce679d87fc48f5
Robert Dunne <robe_dunne@hotmail.com>
2019-11-26 16:18:53 +0000

    move

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
+  app.on('push', async context => {
+    app.log(context)
+  })
+  
   app.on('issues.opened', async context => {
     app.log(context)
     // const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     // return context.github.issues.createComment(issueComment)
   });
 
-  app.on('push', async context => {
-    app.log(context)
-  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
