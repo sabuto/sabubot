89c45115c4fd242466c95197964ea25ef2da0817
Robert Dunne <robe_dunne@hotmail.com>
2019-12-02 15:57:10 +0000

    changed commands and app

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const commands = require('probot-commands')
+// const commands = require('probot-commands')
 const Labeler = require("./lib/labeler")
 const Assigner = require("./lib/assigner")
+const Command = require("./lib/command")
 
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = robot => {
+module.exports = app => {
   // Your code here
-  robot.log('Yay, the app was loaded!')
+  app.log('Yay, the app was loaded!')
 
-  commands(robot, 'label', (context, command) => {
-    robot.log("command", command)
-  })
+  // commands(robot, 'label', (context, command) => {
+  //   robot.log("command", command)
+  // })
 
   // app.on('push', async context => {
   //   app.log(context)
   // });
 
-  robot.on('issues.opened', label)
-  robot.on('issues.edited', label)
+  app.on('issues.opened', label)
+  app.on('issues.edited', label)
+  app.on('issue_comment.*', comment)
   // app.on('issues.opened', async context => { 
   // 	// app.log(context.payload.issue.number)   
   //   // const issueComment = context.issue({ body: 'Thank you for contributing to the repo, someone will be along shortly for some more information or a fix!' })
@@ -34,13 +36,15 @@ module.exports = robot => {
   // https://probot.github.io/docs/development/
 
  async function comment(context) {
-   commands(robot, 'close', (context, command) => {
-     robot.log("command Recieved", command)
-   })
+   const command = new Command(app, context)
+
+   await command.init()
+
+   await command.processCommand()
  }
 
   async function label(context) {
-	const labeler = new Labeler(robot, context)
+	const labeler = new Labeler(app, context)
 
   	await labeler.init()
 
