7a83010d10ee2cc77cff650e8e5546589a77b5bb
Sabuto <robe_dunne@hotmail.com>
2020-01-06 16:27:27 +0000

    updated test

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,11 @@
+https://github.com/andrewlock/auto-assign-issues/blob/master/test/auto-assign-issue.test.ts
+
 const nock = require('nock')
 // Requiring our app implementation
 const myProbotApp = require('..')
 const { Probot } = require('probot')
 // Requiring our fixtures
 const payload = require('./fixtures/issues.opened')
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
 const fs = require('fs')
 const path = require('path')
 
@@ -27,22 +28,20 @@ describe('My Probot app', () => {
     probot.load(myProbotApp)
   })
 
-  test('creates a comment when an issue is opened', async () => {
+  test('assigns repo owner when issue is created or edited when no assignee is present', async () => {
+    const ownerAssignedBody = { 'assignees': ['sabuto'] }
+
     // Test that we correctly return a test token
     nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+      .get('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test'})
 
-    // Test that a comment is posted
     nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
-        expect(body).toMatchObject(issueCreatedBody)
+      .get('/repos/sabuto/bot-test/issues/25', (body) => {
+        expect(body).toMatchObject(ownerAssignedBody)
         return true
       })
       .reply(200)
-
-    // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
   })
 
   afterEach(() => {
